6f671f05d40b74025e4563f067051335605d62e0
mdmhrz <razufreelence@gmail.com>
2025-02-25 18:22:35 +0600

    Some Logical Proble Solved

--- scripts/addmoney.js
+++ scripts/addmoney.js
@@ -1,18 +1,21 @@
-document.getElementById('addmoney-btn').addEventListener('click', function(event){
+document.getElementById('addmoney-btn').addEventListener('click', function (event) {
     event.preventDefault();
     const balance = document.getElementById('main-balance').innerText;
     const mainBalance = parseFloat(balance);
     const pin = document.getElementById('pin').value;
     const convertedPin = parseInt(pin);
     const amount = document.getElementById('add-amount').value;
-    const addAmount = parseFloat(amount); 
+    const addAmount = parseFloat(amount);
     if (convertedPin === 1234) {
-        document.getElementById('main-balance').innerText = mainBalance + addAmount;
+        if ( addAmount >0 ) {
+            document.getElementById('main-balance').innerText = mainBalance + addAmount;
+        }
+        else {
+            alert('Plese enter an amount to add.')
+        }
     }
-    else{
+    else {
         alert("Please Enter Valid PIN Number")
     }
 
-    
-    
 })
